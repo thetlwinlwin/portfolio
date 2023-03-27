bc9fc0837f70ac37ddde15e47a9681e1c484db9a
thetlwinlwin <minthantlwin8@gmail.com>
2023-03-27 22:48:12 +0630

    added scroll feature
    added mock text and scroll feature

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,6 +1,8 @@
 import './home.css';
 import NavBar from '../nav_bar/nav_bar';
 import Responsive from "../portfolio/responsive/responsive";
+import useInView from '../intersection_hook';
+import { useEffect, useRef } from 'react';
 
 const homeText = [
     { title: 'Who We Are?' },
@@ -8,28 +10,56 @@ const homeText = [
 ]
 
 function HomePage() {
+    const homeEndRef = useRef(null);
+    const isInView = useInView(homeEndRef, {
+        rootMargin: "0px",
+        threshold: 0.8,
+    });
+
+    console.log(isInView);
+
+
+
     return (
         <>
             <NavBar />
             <div className="container">
-                <div className="section home">
+                <div className="section center">
                     <span className='home-text'>{homeText[0].title}</span>
                 </div>
-                <div className="section home">
-                    <span className='home-text'>{homeText[1].title}</span>
+                <div ref={homeEndRef} className={`center home-end ${isInView ? 'home-end-show' : ''}`}>
+                    <div className='home-end-wrapper'>
+                        <span className='home-text'>{homeText[1].title}</span>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius elit quis diam efficitur, et sollicitudin metus scelerisque. Donec ac ornare augue. Vestibulum gravida augue sapien. Maecenas a purus felis. Curabitur feugiat tincidunt urna, at suscipit magna. Vestibulum pretium turpis non libero malesuada, eget aliquam urna laoreet. Donec suscipit suscipit posuere.
+
+                        </p>
+                    </div>
                 </div>
-                <div className="section home">
-                    <span className="home-text">
-                        Our Clients
-                    </span>
+                <div className="section center">
+                    <div className='home-end-wrapper'>
+                        <span className='home-text'> Our Clients</span>
+                        <p>
+                            Apple
+                        </p>
+                        <p>
+                            Grab
+                        </p>
+                        <p>
+                            1 Stop Solar
+                        </p>
+                        <p>
+                            Aim High
+                        </p>
+                    </div>
                 </div>
                 <Responsive />
-                <div className="section home">
+                <div className="section center">
                     <span className="home-text">
                         Section 5
                     </span>
                 </div>
-                <div className="section home" >
+                <div className="section center" >
                     <span className="home-text">
                         Section 6
                     </span>
