45e19e6257342881910985de5292d658552bd618
thetlwinlwin <minthantlwin8@gmail.com>
2023-03-29 22:52:18 +0630

    change the intersection effect

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -2,7 +2,7 @@ import './home.css';
 import NavBar from '../nav_bar/nav_bar';
 import Responsive from "../portfolio/responsive/responsive";
 import useInView from '../intersection_hook';
-import { useRef } from 'react';
+import { useMemo, useRef } from 'react';
 
 const homeText = [
     { title: 'Who We Are?' },
@@ -10,34 +10,37 @@ const homeText = [
 ]
 
 function HomePage() {
+    const option = useMemo(() => ({ rootMargin: '0px', threshold: 0.7, }), [])
+
     const homeEndRef = useRef(null);
-    const isInView = useInView(homeEndRef, {
-        rootMargin: "0px",
-        threshold: 1,
-    });
+    const clientRef = useRef(null);
+    const responsiveRef = useRef(null);
 
-    console.log(isInView);
+    const isHomeEndInView = useInView(homeEndRef, option);
+    const isClientInView = useInView(clientRef, option);
+    const isResponsiveInView = useInView(responsiveRef, option);
 
+    console.log(isResponsiveInView);
 
     return (
         <>
             <NavBar />
             <div className="container">
                 <div className='section'>
-                    <span className='home-text'>{homeText[0].title}</span>
+                    <span className='title-text'>{homeText[0].title}</span>
                 </div>
-                <div ref={homeEndRef} className={`section check hide${isInView ? 'show' : ''}`}>
-                    <div className='home-end-wrapper'>
-                        <span className='home-text'>{homeText[1].title}</span>
+                <div ref={homeEndRef} className={`section hide${isHomeEndInView ? 'show' : ''}`}>
+                    <div className='wrapper home-end-gap'>
+                        <span className='title-text'>{homeText[1].title}</span>
                         <p className='para-text'>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius elit quis diam efficitur, et sollicitudin metus scelerisque. Donec ac ornare augue. Vestibulum gravida augue sapien. Maecenas a purus felis. Curabitur feugiat tincidunt urna, at suscipit magna. Vestibulum pretium turpis non libero malesuada, eget aliquam urna laoreet. Donec suscipit suscipit posuere.
 
                         </p>
                     </div>
                 </div>
-                <div className='section'>
-                    <div className='client-wrapper'>
-                        <span className='home-text'> Our Clients</span>
+                <div ref={clientRef} className={`section hide${isClientInView ? 'show' : ''}`}>
+                    <div className='wrapper client-gap'>
+                        <span className='title-text'> Our Clients</span>
                         <p className='para-text'>
                             Apple
                         </p>
@@ -52,14 +55,16 @@ function HomePage() {
                         </p>
                     </div>
                 </div>
-                <Responsive />
+                <div ref={responsiveRef} className={`section hide${isResponsiveInView ? 'show' : ''}`}>
+                    < Responsive />
+                </div>
                 <div className="section ">
-                    <span className="home-text">
+                    <span className="title-text">
                         Section 5
                     </span>
                 </div>
                 <div className="section " >
-                    <span className="home-text">
+                    <span className="title-text">
                         Section 6
                     </span>
                 </div>
