a4aa3e435a33c93ce62f4276770b88d8d03e934f
thetlwinlwin <minthantlwin8@gmail.com>
2023-04-11 16:25:31 +0630

    refactor scroll behaviour

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -10,7 +10,7 @@ const homeText = [
 ]
 
 function HomePage() {
-    const option = useMemo(() => ({ rootMargin: '0px', threshold: 0.8, }), [])
+    const option = useMemo(() => ({ rootMargin: '0px', threshold: 0.7, }), [])
 
     const homeEndRef = useRef(null);
     const clientRef = useRef(null);
@@ -29,8 +29,8 @@ function HomePage() {
                 <div className='section'>
                     <span className='title-text'>{homeText[0].title}</span>
                 </div>
-                <div ref={homeEndRef} className={`section hide${isHomeEndInView ? 'show' : ''}`}>
-                    <div className='wrapper home-end-gap'>
+                <div ref={homeEndRef} className={`section intro-height hide${isHomeEndInView ? 'show' : ''}`}>
+                    <div className="wrapper home-end-gap">
                         <span className='title-text'>{homeText[1].title}</span>
                         <p className='para-text'>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius elit quis diam efficitur, et sollicitudin metus scelerisque. Donec ac ornare augue. Vestibulum gravida augue sapien. Maecenas a purus felis. Curabitur feugiat tincidunt urna, at suscipit magna. Vestibulum pretium turpis non libero malesuada, eget aliquam urna laoreet. Donec suscipit suscipit posuere.
@@ -38,8 +38,8 @@ function HomePage() {
                         </p>
                     </div>
                 </div>
-                <div ref={clientRef} className={`section hide${isClientInView ? 'show' : ''}`}>
-                    <div className='wrapper client-gap'>
+                <div ref={clientRef} className={`section intro-height hide${isClientInView ? 'show' : ''}`} >
+                    <div className="wrapper client-gap">
                         <span className='title-text'> Our Clients</span>
                         <p className='para-text'>
                             Apple
